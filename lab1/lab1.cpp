d951906ed756f226b31524ec482f460752abd8c4
willis12rus <49200813+willis12rus@users.noreply.github.com>
2021-09-11 16:37:19 +0300

    add lab1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,28 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
+{
+	cout << (to_string((int)lpParam) + "\n");
+	ExitThread(0); // функция устанавливает код завершения потока в 0
+}
+
+int main(int argc, CHAR* argv[])
+{
+	int param = atoi(argv[1]);
+
+	HANDLE* handles = new HANDLE[param];
+	for (int i = 0; i < param; i++) {
+		handles[i] = CreateThread(NULL, 0, &ThreadProc, LPVOID(i), CREATE_SUSPENDED, NULL);
+	}
+
+	for (int i = 0; i < param; i++) {
+		ResumeThread(handles[i]);
+	}
+
+	WaitForMultipleObjects(param, handles, true, INFINITE);
+	return 0;
+}
